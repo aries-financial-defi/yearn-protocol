f2d604f75be1e7f5983e9f10c8b45d60296dd12e
poa010101 <70993984+poa010101@users.noreply.github.com>
2020-10-28 08:09:34 +0800

    mainnet deployment

--- contracts/controllers/Controller.sol
+++ contracts/controllers/Controller.sol
@@ -1,15 +1,161 @@
+/**
+ *Submitted for verification at Etherscan.io on 2020-08-11
+*/
+/**
+ *Submitted for verification at Etherscan.io on 2020-07-26
+*/
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.5.16;
 
-import "@openzeppelinV2/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelinV2/contracts/math/SafeMath.sol";
-import "@openzeppelinV2/contracts/utils/Address.sol";
-import "@openzeppelinV2/contracts/token/ERC20/SafeERC20.sol";
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
-import "../../interfaces/yearn/Converter.sol";
-import "../../interfaces/yearn/OneSplitAudit.sol";
-import "../../interfaces/yearn/Strategy.sol";
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+
+        return c;
+    }
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+}
+
+library Address {
+    function isContract(address account) internal view returns (bool) {
+        bytes32 codehash;
+        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
+        // solhint-disable-next-line no-inline-assembly
+        assembly { codehash := extcodehash(account) }
+        return (codehash != 0x0 && codehash != accountHash);
+    }
+    function toPayable(address account) internal pure returns (address payable) {
+        return address(uint160(account));
+    }
+    function sendValue(address payable recipient, uint256 amount) internal {
+        require(address(this).balance >= amount, "Address: insufficient balance");
+
+        // solhint-disable-next-line avoid-call-value
+        (bool success, ) = recipient.call.value(amount)("");
+        require(success, "Address: unable to send value, recipient may have reverted");
+    }
+}
+
+library SafeERC20 {
+    using SafeMath for uint256;
+    using Address for address;
+
+    function safeTransfer(IERC20 token, address to, uint256 value) internal {
+        callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
+    }
+
+    function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
+        callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
+    }
+
+    function safeApprove(IERC20 token, address spender, uint256 value) internal {
+        require((value == 0) || (token.allowance(address(this), spender) == 0),
+            "SafeERC20: approve from non-zero to non-zero allowance"
+        );
+        callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
+    }
+    function callOptionalReturn(IERC20 token, bytes memory data) private {
+        require(address(token).isContract(), "SafeERC20: call to non-contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = address(token).call(data);
+        require(success, "SafeERC20: low-level call failed");
+
+        if (returndata.length > 0) { // Return data is optional
+            // solhint-disable-next-line max-line-length
+            require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
+        }
+    }
+}
+
+interface Strategy {
+    function want() external view returns (address);
+    function deposit() external;
+    function withdraw(address) external;
+    function withdraw(uint) external;
+    function withdrawAll() external returns (uint);
+    function balanceOf() external view returns (uint);
+}
+
+interface Converter {
+    function convert(address) external returns (uint);
+}
+
+interface OneSplitAudit {
+    function swap(
+        address fromToken,
+        address destToken,
+        uint256 amount,
+        uint256 minReturn,
+        uint256[] calldata distribution,
+        uint256 flags
+    )
+    external
+    payable
+    returns(uint256 returnAmount);
+
+    function getExpectedReturn(
+        address fromToken,
+        address destToken,
+        uint256 amount,
+        uint256 parts,
+        uint256 flags // See constants in IOneSplit.sol
+    )
+    external
+    view
+    returns(
+        uint256 returnAmount,
+        uint256[] memory distribution
+    );
+}
 
 contract Controller {
     using SafeERC20 for IERC20;
@@ -27,8 +173,8 @@ contract Controller {
 
     mapping(address => mapping(address => bool)) public approvedStrategies;
 
-    uint256 public split = 500;
-    uint256 public constant max = 10000;
+    uint public split = 500;
+    uint public constant max = 10000;
 
     constructor(address _rewards) public {
         governance = msg.sender;
@@ -47,7 +193,7 @@ contract Controller {
         strategist = _strategist;
     }
 
-    function setSplit(uint256 _split) public {
+    function setSplit(uint _split) public {
         require(msg.sender == governance, "!governance");
         split = _split;
     }
@@ -78,11 +224,7 @@ contract Controller {
         approvedStrategies[_token][_strategy] = false;
     }
 
-    function setConverter(
-        address _input,
-        address _output,
-        address _converter
-    ) public {
+    function setConverter(address _input, address _output, address _converter) public {
         require(msg.sender == strategist || msg.sender == governance, "!strategist");
         converters[_input][_output] = _converter;
     }
@@ -98,7 +240,7 @@ contract Controller {
         strategies[_token] = _strategy;
     }
 
-    function earn(address _token, uint256 _amount) public {
+    function earn(address _token, uint _amount) public {
         address _strategy = strategies[_token];
         address _want = Strategy(_strategy).want();
         if (_want != _token) {
@@ -112,7 +254,7 @@ contract Controller {
         Strategy(_strategy).deposit();
     }
 
-    function balanceOf(address _token) external view returns (uint256) {
+    function balanceOf(address _token) external view returns (uint) {
         return Strategy(strategies[_token]).balanceOf();
     }
 
@@ -121,7 +263,7 @@ contract Controller {
         Strategy(strategies[_token]).withdrawAll();
     }
 
-    function inCaseTokensGetStuck(address _token, uint256 _amount) public {
+    function inCaseTokensGetStuck(address _token, uint _amount) public {
         require(msg.sender == strategist || msg.sender == governance, "!governance");
         IERC20(_token).safeTransfer(msg.sender, _amount);
     }
@@ -131,32 +273,24 @@ contract Controller {
         Strategy(_strategy).withdraw(_token);
     }
 
-    function getExpectedReturn(
-        address _strategy,
-        address _token,
-        uint256 parts
-    ) public view returns (uint256 expected) {
-        uint256 _balance = IERC20(_token).balanceOf(_strategy);
+    function getExpectedReturn(address _strategy, address _token, uint parts) public view returns (uint expected) {
+        uint _balance = IERC20(_token).balanceOf(_strategy);
         address _want = Strategy(_strategy).want();
-        (expected, ) = OneSplitAudit(onesplit).getExpectedReturn(_token, _want, _balance, parts, 0);
+        (expected,) = OneSplitAudit(onesplit).getExpectedReturn(_token, _want, _balance, parts, 0);
     }
 
     // Only allows to withdraw non-core strategy tokens ~ this is over and above normal yield
-    function yearn(
-        address _strategy,
-        address _token,
-        uint256 parts
-    ) public {
+    function yearn(address _strategy, address _token, uint parts) public {
         require(msg.sender == strategist || msg.sender == governance, "!governance");
         // This contract should never have value in it, but just incase since this is a public call
-        uint256 _before = IERC20(_token).balanceOf(address(this));
+        uint _before = IERC20(_token).balanceOf(address(this));
         Strategy(_strategy).withdraw(_token);
-        uint256 _after = IERC20(_token).balanceOf(address(this));
+        uint _after =  IERC20(_token).balanceOf(address(this));
         if (_after > _before) {
-            uint256 _amount = _after.sub(_before);
+            uint _amount = _after.sub(_before);
             address _want = Strategy(_strategy).want();
-            uint256[] memory _distribution;
-            uint256 _expected;
+            uint[] memory _distribution;
+            uint _expected;
             _before = IERC20(_want).balanceOf(address(this));
             IERC20(_token).safeApprove(onesplit, 0);
             IERC20(_token).safeApprove(onesplit, _amount);
@@ -165,14 +299,14 @@ contract Controller {
             _after = IERC20(_want).balanceOf(address(this));
             if (_after > _before) {
                 _amount = _after.sub(_before);
-                uint256 _reward = _amount.mul(split).div(max);
+                uint _reward = _amount.mul(split).div(max);
                 earn(_want, _amount.sub(_reward));
                 IERC20(_want).safeTransfer(rewards, _reward);
             }
         }
     }
 
-    function withdraw(address _token, uint256 _amount) public {
+    function withdraw(address _token, uint _amount) public {
         require(msg.sender == vaults[_token], "!vault");
         Strategy(strategies[_token]).withdraw(_amount);
     }
